7f60005811cde032f133ed3f68516558fd7d7943
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-09-09 12:06:40 +0900

    release

--- data-platform-inspection-plan-sql-header-doc-data.sql
+++ data-platform-inspection-plan-sql-header-doc-data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `data_platform_inspection_plan_header_doc_data`
 (
   `InspectionPlan`                 int(16) NOT NULL,
-  `DocType`                        varchar(20) NOT NULL,
+  `DocType`                        varchar(100) NOT NULL,
   `DocVersionID`                   int(4) NOT NULL,
   `DocID`                          varchar(100) NOT NULL,
   `FileExtension`                  varchar(20) NOT NULL,
